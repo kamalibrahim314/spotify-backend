38bbcd8e5bd0a9c1ef0cb5877ad6e6c36928bafc
kamal ibrahim <ibrahimkamal314@gmail.com>
2025-09-29 14:01:47 +0300

    last version solce mongo

--- src/config/connDB.js
+++ src/config/connDB.js
@@ -2,10 +2,15 @@ import mongoose from "mongoose";
 
 export const connDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URL);
-        console.log(`MongoDB connected: ${conn.connection.host}`);
+        const conn = await mongoose.connect(
+            process.env.MONGODB_URL || 'mongodb+srv://ibrahimkamal314_db_user:zCuLs53aUZlZ5cyP@cluster0.zfohi48.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("✅ MongoDB connected");
     } catch (error) {
-        console.log(error);
+        console.error("❌ MongoDB connection failed:", error.message);
+        process.exit(1);
     }
 };
 
